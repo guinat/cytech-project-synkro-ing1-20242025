3257559e427afa4738e601b1b0717809bacb1705
guinat <guinat@users.noreply.github.com>
2025-02-12 20:46:00 +0100

    ex: Hello2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <HelloWorld />
-      <p className='text-fuchsia-600'>Hello!</p>
+      <p className='text-amber-600'>Hello 2!</p>
     </div>
   );
 };
