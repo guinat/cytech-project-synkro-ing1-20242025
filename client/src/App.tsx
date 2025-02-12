8d243f2e755cae9ce7ef0283dcf4c7fa3a2e38e1
guinat <guinat@users.noreply.github.com>
2025-02-12 20:47:36 +0100

    update: App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,6 @@ const App = () => {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <HelloWorld />
-      <p className='text-amber-600'>Hello 2!</p>
     </div>
   );
 };
